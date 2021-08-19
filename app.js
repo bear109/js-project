d9b991cd3c076144ce62e6286e7626d629728d7b
iwinix109 <iwinix109@naver.com>
2021-08-19 14:24:54 +0900

    event 처리

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const loginForm = document.querySelector('#login-form');
 const loginInput = loginForm.querySelector('input');
-const loginButton = loginForm.querySelector('button');
+const greeting = document.querySelector('#greeting');
 
-function onLoginBtnClick() {
+const HIDDEN_CLASSNAME = 'hidden';
+
+function onLoginSubmit(event) {
+  event.preventDefault();
   const username = loginInput.value;
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginButton.addEventListener('click', onLoginBtnClick);
+loginForm.addEventListener('submit', onLoginSubmit);
